520dbeac96d23652d0c393c26902d721655534dd
xballes <70892571+xballes@users.noreply.github.com>
2023-11-14 18:16:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -14,3 +14,8 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+
+def test_setbalance1000():
+    obj = Wallet(0)
+    obj.set_balance1000()
+    assert obj.get_balance() == 1000
